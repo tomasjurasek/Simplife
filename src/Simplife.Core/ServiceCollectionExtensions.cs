0c1c267a9ac1fc2d032dca52e2cae1926e9572f6
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2023-11-19 20:49:39 +0100

    wrap

---
+++ src/Simplife.Core/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Simplife.Core.Events;
+
+namespace Simplife.Core
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSimplife(this IServiceCollection services)
+        {
+            EventDispatcher.Init();
+            return services;
+        }
+    }
+}
