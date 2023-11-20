5971df451b9eeab076ee99db8d9d8a5ed4c3e984
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2023-11-20 13:43:42 +0100

    event bus

--- src/Simplife.Core/Events/IEventHandler.cs
+++ src/Simplife.Core/Events/IEventHandler.cs
@@ -2,6 +2,6 @@
 {
     public interface IEventHandler<TEvent> where TEvent : IEvent
     {
-        Task HandleAsync(TEvent @event);
+        Task HandleAsync(TEvent @event, CancellationToken cancellationToken  = default);
     }
 }
