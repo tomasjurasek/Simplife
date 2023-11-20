8f33dac9b1127e4f4b52e588383c3b779e96b254
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2023-11-20 13:32:15 +0100

    event bus

---
+++ src/Simplife.Core/Events/IEventBus.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Simplife.Core.Events
+{
+    public interface IEventBus
+    {
+        Task PublishAsync(IEnumerable<IEvent> events, CancellationToken cancellationToken = default);
+    }
+
+
+    class InMemoryEventBus : IEventBus
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public InMemoryEventBus(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task PublishAsync(IEnumerable<IEvent> events, CancellationToken cancellationToken = default)
+        {
+            foreach (var @event in events)
+            {
+                var eventHandlerType = typeof(IEventHandler<>)
+                    .MakeGenericType(@event.GetType());
+
+                var eventHandlers = _serviceProvider.GetServices(eventHandlerType);
+
+                foreach (var eventHandler in eventHandlers)
+                {
+                    var methodInfo = eventHandlerType.GetMethod(nameof(IEventHandler<IEvent>.HandleAsync));
+                    await (Task)methodInfo!.Invoke(eventHandler, [@event, cancellationToken])!;
+                }
+            }
+        }
+    }
+}
---
+++ src/Simplife.Core/Events/IEventHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace Simplife.Core.Events
+{
+    public interface IEventHandler<TEvent> where TEvent : IEvent
+    {
+        Task HandleAsync(TEvent @event);
+    }
+}
