c21e677b0fef879d5b80f967e7900896c738130e
tomas.jurasek <jurasek.tomas@hotmail.cz>
2024-06-22 14:04:50 +0200

    aspire

--- src/Simplife.Domain/Events/IEventHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Simplife.Domain.Events
-{
-    public interface IEventHandler<TEvent> where TEvent : IEvent
-    {
-        Task HandleAsync(TEvent @event, CancellationToken cancellationToken  = default);
-    }
-}
